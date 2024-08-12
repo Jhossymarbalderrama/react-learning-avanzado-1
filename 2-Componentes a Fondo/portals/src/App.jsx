3cd0d0fe109583e004adb89307dd539232fedcf5
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-12 19:03:49 -0300

    2- Implementando Portals en React

--- 2-Componentes a Fondo/portals/src/App.jsx
+++ 2-Componentes a Fondo/portals/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
+
 import Agenda from './components/Agenda'
 
 function App() {
--- 2-Componentes a Fondo/portals/src/components/Agenda.jsx
+++ 2-Componentes a Fondo/portals/src/components/Agenda.jsx
@@ -1,26 +1,45 @@
-import React, { Component } from 'react'
-import  ReservarHora from './ReservarHora'
+import React, { Component } from "react";
+import Modal from "./Modal";
+import ReservarHora from "./ReservarHora";
+import Confirmacion from "./Confirmacion";
 
 export default class Agenda extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = { modalVisible: false };
+  }
 
-    confirm = () => {
-        alert("Ok")
-    }
+   modalClickHandler = (e) => {
+    this.setState({ modalVisible: false });
+  };
+  confirm = () => {
+    this.setState({ modalVisible: true });
+  };
 
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <h1>Doctor Nicolás</h1>
-                    <ReservarHora onClick={this.confirm} />
-                    <ReservarHora onClick={this.confirm} />
-                    <ReservarHora onClick={this.confirm} />
-                    <ReservarHora onClick={this.confirm} />
-                    <ReservarHora onClick={this.confirm} />
-                    <ReservarHora onClick={this.confirm} />
-                    <ReservarHora onClick={this.confirm} />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1>Doctor Nicolás</h1>
+
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+          <ReservarHora onClick={this.confirm}   />
+
+        </div>
+
+        <div onClick={this.modalClickHandler}>
+          <Modal>{this.state.modalVisible && <Confirmacion />}</Modal>
+        </div>
+        
+      </div>
+    );
+  }
 }
