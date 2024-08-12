1d06f0cb3caaf6c51e3f7ac0d8e4fa7817f5b33d
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-08-12 17:07:43 -0300

    Manejo de Errores en React con TryCach

---
+++ 2-Componentes a Fondo/error_handling/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import MiComponente from './components/MiComponente'
+
+function App() {
+
+  return (
+    <>
+      <MiComponente/>
+      <MiComponente/>
+      <MiComponente/>
+    </>
+  )
+}
+
+export default App
---
+++ 2-Componentes a Fondo/error_handling/src/components/MiComponente.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import MiError from './MiError'
+import { useState } from 'react';
+
+export default function MiComponente(props) {
+
+    const [visibleError, setVisibleError] = useState(false);
+
+    const comprarProducto = (e) => {
+        try {
+            e.metodoIncorrecto();
+        } catch (error) {
+            diplayError();
+        }
+    }
+
+    const diplayError = () => {
+        setVisibleError(true);
+    }
+
+    return (
+        <div className='col-md-4'>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+
+                    <button onClick={comprarProducto} className='btn btn-primary'>
+                        Comprar
+                    </button>
+
+                    {visibleError && <MiError/>}
+                </div>
+            </div>
+        </div>
+    )
+}
